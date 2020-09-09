53c89efbf5bb66f14eae78c4dae1a9cc8c4f04a3
Gurinderpal Singh <gurinder.narang88@gmail.com>
2020-09-09 18:22:27 +0530

    Navigation added

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
-	private
-	def sign_up_params
-		params.require(:user).permit(:email, :user_name, :password, :password_confirmation)
-	end
-	def account_update_params
-		params.require(:user).permit(:email, :user_name, :password, :password_confirmation, :current_password)
-	end
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :user_name, :password, :password_confirmation, :user_role)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :user_name, :password, :password_confirmation, :current_password, :user_role)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  DEFAULT_ROLES = %w(Employee Supervisor Translator)
   validates :user_name, presence: true, length: {minimum: 4, maximum: 16}
+  validates :user_role, presence: true, inclusion: {in: DEFAULT_ROLES}
 end
